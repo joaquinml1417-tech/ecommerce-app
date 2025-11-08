0ba9cb03f5d74a6e93799dd6328cdbb017b5fa64
joaquinml1417-tech <joaquinml1417@gmail.com>
2025-11-07 21:13:31 -0300

    Update logout.js

--- AWTP1/js/logout.js
+++ AWTP1/js/logout.js
@@ -3,12 +3,10 @@ document.addEventListener("DOMContentLoaded", function () {
   if (logoutBtn) {
     logoutBtn.addEventListener("click", function (e) {
       e.preventDefault();
-
-      // Detectar si estás en index.html (raíz) o en pages/
       const isRoot = window.location.pathname.endsWith("index.html") || window.location.pathname === "/";
       const loginPath = isRoot ? "pages/login.html" : "login.html";
-
       window.location.href = loginPath;
     });
   }
 });
+
